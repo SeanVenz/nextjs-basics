cf7057a6144d91a0d499cab4e97eb2cd150792d2
SeanVenz <seanvenz.quijano@cit.edu>
2023-10-30 15:59:28 +0800

    added

---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import { Albert_Sans, Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+export const albert = Albert_Sans({subsets: ['latin']});
+export const lusitana = Lusitana({subsets: ['latin'], weight: ['700']});
